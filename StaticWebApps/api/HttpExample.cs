c965733deeee49d1ad3e16dd976d7b4efd240de8
Nitin Manju <nitin.manju@kongsbergdigital.com>
2022-09-07 23:28:17 +0530

    Adding API project

---
+++ StaticWebApps/api/HttpExample.cs
@@ -0,0 +1,20 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+namespace api
+{
+    public static class HttpExample
+    {
+        [FunctionName("HttpExample")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {            
+            return new JsonResult("Hello from the function API!");
+        }
+    } 
+}
